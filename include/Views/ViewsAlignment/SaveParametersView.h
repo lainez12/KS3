ea84f13d0081dae617b696ab6abe560af31d69c8
romainb <bry@kloe-france.com>
2026-06-11 09:22:14 +0200

    Fix crash on application close. `BaseViewModel` dtor now has the virtual attribute.

--- include/Views/ViewsAlignment/SaveParametersView.h
+++ include/Views/ViewsAlignment/SaveParametersView.h
@@ -8,14 +8,17 @@
 #include "Views/ViewBase.h"
 #include <Views/KeyboardConnections.h>
 
-namespace Ui {
+namespace Ui
+{
     class SaveParametersView;
 } // namespace UI
 
-namespace Kub3::UI::Views::ViewsAlignment {
-    class SaveParametersView final : public AlignmentViewBase {
-        using SaveParametersViewModel = Kub3::UI::ViewModels::ViewModelsAlignment::SaveParametersViewModel;
+namespace Kub3::UI::Views::ViewsAlignment
+{
+    using SaveParametersViewModel = Kub3::UI::ViewModels::ViewModelsAlignment::SaveParametersViewModel;
 
+    class SaveParametersView final : public AlignmentViewBase
+    {
         Q_OBJECT
 
     public:
--- src/Views/ViewsAlignment/SaveParametersView.cpp
+++ src/Views/ViewsAlignment/SaveParametersView.cpp
@@ -10,23 +10,31 @@
 SaveParametersView::SaveParametersView(Unique<SaveParametersViewModel> viewModel, QWidget *parent) :
     AlignmentViewBase(std::move(viewModel), parent),
     m_keyboard(this),
-    ui(new Ui::SaveParametersView) {
+    ui(new Ui::SaveParametersView)
+{
     ui->setupUi(this);
     m_keyboard.setupKeyboardConnections(this);
     setDefaultTitleBar("Exposure Settings");
 }
-SaveParametersView::~SaveParametersView() {
+
+SaveParametersView::~SaveParametersView()
+{
+    delete ui;
 }
 
-void SaveParametersView::resizeEvent(QResizeEvent *ev) {
+void SaveParametersView::resizeEvent(QResizeEvent *ev)
+{
     QWidget::resizeEvent(ev);
 }
 
-void SaveParametersView::setNewNavButtonsConfigs() {
+void SaveParametersView::setNewNavButtonsConfigs()
+{
 }
 
-void SaveParametersView::onBackButtonClicked(const QString &buttonId) {
+void SaveParametersView::onBackButtonClicked(const QString &buttonId)
+{
 }
 
-void SaveParametersView::onValidateButtonClicked(const QString &buttonId) {
+void SaveParametersView::onValidateButtonClicked(const QString &buttonId)
+{
 }
